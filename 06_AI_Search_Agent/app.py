7cda642166b763947050985873c1539412b621ff
beniamine3155 <beniamine3155@gmail.com>
2025-08-09 23:57:36 +0600

    implement streamlit ui

---
+++ 06_AI_Search_Agent/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from search_agent import query_documents
+
+st.title("AI-powered Enterprise Search Agent")
+st.caption("Ask questions across internal files, policies, or wikis")
+
+query = st.text_input("Enter your question..... )")
+
+if query:
+    result = query_documents(query)
+    st.subheader("AI-Generated Answer")
+    st.text_area("Search Response", result, height=500)
